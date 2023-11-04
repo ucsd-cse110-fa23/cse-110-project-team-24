4ed57a15083d6b78ae64d63ca64b40c3197f1fb8
Robintianqili <til024@ucsd.edu>
2023-11-04 15:39:43 -0700

    Adding RecipeList class

---
+++ src/PantryPal/RecipeList.java
@@ -0,0 +1,13 @@
+package PantryPal;
+import javafx.scene.layout.VBox;
+
+class RecipeList extends VBox {
+    RecipeList(){
+        this.setSpacing(5); // sets spacing between tasks
+        this.setPrefSize(500, 560);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+    }
+    public void removeSelectedRecipes() {
+        this.getChildren().removeIf(task -> task instanceof Recipe && ((Recipe) task).getDeleteit());
+    }
+}
