a9b51e4f0bbac1622d6e0b82d2841f532fe6a119
Weiji Chen <wjchen@ucsd.edu>
2023-11-12 15:07:32 -0800

    US 4 See Detailed View - Footer Class
    - added comments

--- app/src/main/java/pantrypal/Footer.java
+++ app/src/main/java/pantrypal/Footer.java
@@ -4,22 +4,27 @@ import javafx.scene.layout.HBox;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 
+// Footer class extends HBox to arrange its children in a horizontal row
 class Footer extends HBox {
     private Button Create;
 
+    // Constructor for Footer
     Footer() {
         this.setPrefSize(500, 60);
         this.setStyle("-fx-background-color: #F0F8FF;");
         this.setSpacing(15);
 
+        // Style string for the button
         String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #FFFFFF;  -fx-font-weight: bold; -fx-font: 11 arial;";
 
+         // Initialize the 'Create New Recipe' button and apply style
         Create = new Button("Create New Recipe");
         Create.setStyle(defaultButtonStyle);
         this.getChildren().add(Create);
         this.setAlignment(Pos.CENTER);
     }
     
+    /// Method to get the Create button
     public Button getCreateButton(){
         return this.Create;
     }
