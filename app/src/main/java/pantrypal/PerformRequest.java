ca768e0ec648fd3561b110b83bf24accb3ef16e1
Weiji Chen <wjchen@ucsd.edu>
2023-11-12 15:18:51 -0800

    US 1- 4: Update PerformRequest.java
    - refactor code to be more readable and modular

--- app/src/main/java/pantrypal/PerformRequest.java
+++ app/src/main/java/pantrypal/PerformRequest.java
@@ -1,5 +1,6 @@
 package pantrypal;
 
+//import statements
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -11,9 +12,15 @@ import java.net.URI;
 import java.net.URLEncoder;
 import java.net.URLDecoder;
 
+// Class PerformRequest to handle HTTP requests
 public class PerformRequest {
+
+    // Method to perform an HTTP request with US-ASCII encoding
     public static String performRequest(String endpoint, String method, String data, String query) {
         try {
+
+             // Construct the full URL for the request
+              // Encode the query string if provided
             String urlString = "http://localhost:8100/" + endpoint;
             if (query != null) {
                 /**
@@ -26,9 +33,12 @@ public class PerformRequest {
             }
             URL url = new URI(urlString).toURL();
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+             // Set request method and enable output
             conn.setRequestMethod(method);
             conn.setDoOutput(true);
 
+            // Write the data to the request body, if provided
             if (data != null) {
                 OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
                 out.write(URLEncoder.encode(data, "US-ASCII"));
@@ -36,9 +46,11 @@ public class PerformRequest {
                 out.close();
             }
 
+            // Read the response from the server
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String response = in.readLine();
             in.close();
+            // Return the decoded response
             return URLDecoder.decode(response, "US-ASCII");
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -47,7 +59,12 @@ public class PerformRequest {
     }
 
     // does not use US-ASCII encoding
+
+    // Method to perform a transcription request without US-ASCII encoding
+
    public static String performTranscriptionRequest(String endpoint, String method, String data, String query) throws UnsupportedEncodingException {
+
+        // Encode the query using ISO-8859-1 charset
         query = URLEncoder.encode(query, java.nio.charset.StandardCharsets.ISO_8859_1);
         try {
             String urlString = "http://localhost:8100/" + endpoint;
@@ -55,11 +72,13 @@ public class PerformRequest {
                 urlString += "?=" + query;
             }
             
+             // Create a URL object and open a connection
             URL url = new URI(urlString).toURL();
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod(method);
             conn.setDoOutput(true);
 
+            // Write the data to the request body, if provided
             if (data != null) {
                 OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
                 out.write(data);
@@ -67,9 +86,11 @@ public class PerformRequest {
                 out.close();
             }
 
+            // Read the response from the server
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String response = in.readLine();
             in.close();
+            // Return the response as is
             return response;
         } catch (Exception ex) {
             ex.printStackTrace();
