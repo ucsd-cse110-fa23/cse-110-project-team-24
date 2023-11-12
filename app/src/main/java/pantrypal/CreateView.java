c559de88ecf3d3c1f9572ab01eee610f93253979
Weiji Chen <wjchen@ucsd.edu>
2023-11-12 14:45:52 -0800

    US 4 See Detail View - Refactor Code
    - organized code structure
    -  added necessary comments to clarify functionalities

--- app/src/main/java/pantrypal/CreateView.java
+++ app/src/main/java/pantrypal/CreateView.java
@@ -1,5 +1,6 @@
 package pantrypal;
 
+// (import statements)
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -15,7 +16,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
+
+
+//CreateVie arrange its children through VBox
 public class CreateView extends VBox {
+
+     // UI components declaration
     private Button BackButton;
     private Label Createlabel;
     private Button StartRecording;
@@ -23,9 +29,15 @@ public class CreateView extends VBox {
     private TextArea IngredientList;
     private Button StartFinding;
     private Button EditButton;
+
+    // Flag to toggle editing state
     private boolean editing = false;
 
+    // Constructor
     CreateView() {
+
+        // Initialize UI components with properties like size and style
+        // Adding UI components to the VBox
         BackButton = new Button("Back");
         BackButton.setPrefSize(100, 50);
         BackButton.setStyle("-fx-background-color: #6495ED; -fx-border-width: 0;");
@@ -76,7 +88,11 @@ public class CreateView extends VBox {
         this.getChildren().add(StartFinding);
     }
 
+     // Toggles the editability of the text areas and changes button text and style
     public void SetEditable() {
+
+         // If not currently editing, enable editing and update styles
+          // If editing, disable editing and revert styles
         if (editing == false) {
             this.EditButton.setText("Editing");
             this.EditButton.setStyle("-fx-background-color: #DE3163; -fx-border-width: 0;");
@@ -96,33 +112,42 @@ public class CreateView extends VBox {
         TypeArea.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;");
     }
 
+    // Getter for TypeArea
     public TextArea getTypeArea() {
         return this.TypeArea;
     }
 
+     // Getter for IngredientList
     public TextArea getIngredientList() {
         return this.IngredientList;
     }
 
+     // Creates a new Scene with the provided CreateView instance
     public static Scene CreateScene(CreateView d) {
         Scene secondScene = new Scene(d, 600, 800);
         return secondScene;
     }
 
+    // Creates a new Scene with the provided AudioRecorder instance
     public static Scene CreateScene(AudioRecorder auidos) {
         Scene thirdScene = new Scene(auidos, 800, 200);
         return thirdScene;
     }
 
+    // Opens the CreateView with the ability to record, edit, and find recipes
     public void OpenCreateView(RecipeList taskList) {
         Scene secondScene = CreateScene(this);
         // New window (Stage)
         Stage createViewWindow = new Stage();
         createViewWindow.setTitle("Create Recipe");
         createViewWindow.setScene(secondScene);
+
+        // Set action for EditButton to toggle edit mode
         this.EditButton.setOnAction(e -> {
             this.SetEditable();
         });
+
+         // Set action for BackButton to close the current window
         this.BackButton.setOnAction(e -> {
             try {
                 createViewWindow.close();
@@ -132,6 +157,7 @@ public class CreateView extends VBox {
             }
         });
 
+         // Set action for StartRecording button to open the audio recorder
         this.StartRecording.setOnAction(e -> {
             AudioRecorder recorder = new AudioRecorder();
             Stage recorderWindow = new Stage();
@@ -140,9 +166,11 @@ public class CreateView extends VBox {
             recorderWindow.setScene(thirdScene);
             recorderWindow.show();
             
+            // Set up the audio recorder to handle transcription and UI updates
             recorder.setBackAndTrancriptListener(recorderWindow, this);
         });
 
+        // Set action for StartFinding button to generate and display a recipe
         this.StartFinding.setOnAction(e -> {
             Recipe generatedRecipe = GetGeneratedRecipe(this.getTypeArea().getText(), 
                     this.getIngredientList().getText());
@@ -152,18 +180,46 @@ public class CreateView extends VBox {
         createViewWindow.show();
     }
 
+     // Generates a Recipe object from provided meal type and recipe text
     private static Recipe GetGeneratedRecipe(String mealType, String recipeText) { 
          try {
+
+             // Send a request to generate a recipe based on the provided meal type and recipe text
             String recipe = PerformRequest.performRequest("generator/", "GET", null, mealType + ";" + recipeText);
+            // Convert the response to a Recipe object and return it
             return Recipe.of(recipe);
 
         } catch (Exception e) {
+            // Print stack trace in case of an error
             e.printStackTrace();
             return null;
         }
     
     }
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------- alternative approaches ----------------- //
+// ---------------- saved here in case we need it -----------------//
 
     // public static String GetTypes(String s) {
     //     return s.substring(s.indexOf("i want a ") + 9, s.indexOf("and i have") - 1);
