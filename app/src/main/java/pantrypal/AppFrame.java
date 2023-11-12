82993baa95a86ad0bb7bfcd2fe12e15d966fa8b1
Weiji Chen <wjchen@ucsd.edu>
2023-11-12 14:34:12 -0800

    US-7 Use Voice Input: Refactor Code
    - added new comments
    - re-organized code structure

--- app/src/main/java/pantrypal/AppFrame.java
+++ app/src/main/java/pantrypal/AppFrame.java
@@ -3,13 +3,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 
-public class AppFrame extends BorderPane{
-     private Header header;
-    private Footer footer;
-    private RecipeList taskList;
-    
-    private Button Create;
 
+// Class AppFrame extends BorderPane to create a layout for the app
+public class AppFrame extends BorderPane{
+
+    // Instance variables for different components of the app
+    // Header component of the app
+    private Header header; 
+    // Footer component of the app
+    private Footer footer;
+    // List to display recipes or tasks
+    private RecipeList taskList; 
+    // Button to initiate creation of a new recipe or task
+    private Button Create; 
+    
+
+    // Constructor for AppFrame
     AppFrame(){
         header = new Header();
 
@@ -19,6 +28,7 @@ public class AppFrame extends BorderPane{
         // Initialise the Footer Object
         footer = new Footer();
 
+         // Setting up a ScrollPane for the taskList for scroll function
         ScrollPane s1 = new ScrollPane(taskList);
         s1.setFitToWidth(true);
         s1.setFitToHeight(true);
@@ -34,17 +44,53 @@ public class AppFrame extends BorderPane{
         addListeners();
     }
 
+    // Method to add event listeners to components
+
     public void addListeners(){
         Create.setOnAction(e -> {
-            // Recipe newrecipe = new Recipe();
-            // taskList.getChildren().add(newrecipe);
-            // Button title = newrecipe.getTitle();
-            
-            // title.setOnAction(e1 -> {
-            //     newrecipe.OpenDetailView(newrecipe.getStage(), taskList);
-            // });
+
+            //creates a new CreateView instance and opens it
             CreateView createView = new CreateView();
             createView.OpenCreateView(taskList);
         });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------- alternative approaches ----------------- //
+// ---------------- saved here in case we need it -----------------//
+
+// Recipe newrecipe = new Recipe();
+// taskList.getChildren().add(newrecipe);
+// Button title = newrecipe.getTitle();
+
+// title.setOnAction(e1 -> {
+//     newrecipe.OpenDetailView(newrecipe.getStage(), taskList);
+// });
+
+            
--- app/src/main/java/pantrypal/AudioRecorder.java
+++ app/src/main/java/pantrypal/AudioRecorder.java
@@ -1,5 +1,6 @@
 package pantrypal;
 
+// import necessary libraries and files 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -16,19 +17,26 @@ import java.nio.charset.StandardCharsets;
 
 import javax.sound.sampled.*;
 
+
+// Class AudioRecorder extends VBox to arrange child nodes
 class AudioRecorder extends VBox {
+    // UI components
     private Button backAndTranscriptButton;
     private Button startButton;
     private Button stopButton;
-    private AudioFormat audioFormat;
-    private TargetDataLine targetDataLine;
     private Label recordingLabel;
     private TextField ReminderLabel;
+
+    // Audio recording components
+    private AudioFormat audioFormat;
+    private TargetDataLine targetDataLine;
+
     // Set a default style for buttons and fields - background color, font size,
-    // italics
+    // italics, i.e. Style strings for UI elements
     String defaultButtonStyle = "-fx-border-color: #000000; -fx-font: 13 arial; -fx-pref-width: 175px; -fx-pref-height: 50px;";
     String defaultLabelStyle = "-fx-font: 13 arial; -fx-pref-width: 175px; -fx-pref-height: 50px; -fx-text-fill: red; visibility: hidden";
 
+     // Constructor
     AudioRecorder() {
         // Set properties for the flowpane
         this.setPrefSize(370, 120);
@@ -37,6 +45,7 @@ class AudioRecorder extends VBox {
         // this.setHgap(10);
         // this.setPrefWrapLength(170);
 
+        // Initialize UI components
         // Add the buttons and text fields
         ReminderLabel = new TextField(
                 "Please list the meal type (breakfast, lunch, or dinner) and ingredients in the following format: [meal type], [ingredient 1], [ingredient 2], . . .");
@@ -64,10 +73,12 @@ class AudioRecorder extends VBox {
         addListeners();
     }
 
+    // Getter for backAndTranscriptButton
     public Button getbackAndTranscriptButtonButton() {
         return this.backAndTranscriptButton;
     }
 
+     // Method to add action listeners to buttons
     public void addListeners() {
         // Start Button
         startButton.setOnAction(e -> {
@@ -80,6 +91,7 @@ class AudioRecorder extends VBox {
         });
     }
 
+    // Method to define the audio format for recording
     private AudioFormat getAudioFormat() {
         // the number of samples of audio per second.
         // 44100 represents the typical sample rate for CD-quality audio.
@@ -106,21 +118,27 @@ class AudioRecorder extends VBox {
     }
 
 
+    // Method to set action listener for backAndTranscriptButton
     public void setBackAndTrancriptListener(Stage recorderWindow, CreateView view) {
         this.getbackAndTranscriptButtonButton().setOnAction(e -> {
             recorderWindow.close();
             Path filePath = Paths.get("recording.mp3");
             if (Files.exists(filePath)) {
                 try {
+                    // Read the audio file's data
                     byte[] fileData = GetFileData(new File("recording.mp3"));
+                    // Convert file data to a string for transcription
                     String fileString = new String(fileData, java.nio.charset.StandardCharsets.ISO_8859_1);
                     //String fileDataString = new String(fileData, StandardCharsets.UTF_8);
                     String audioTranscription = PerformRequest.performTranscriptionRequest("transcript/", "GET", null, fileString);
 
+                     // Process the transcription to extract meal type and ingredients
                     String[] words = audioTranscription.split(" ");
 
                     String type = words[0];
                     String IngredientList = audioTranscription.substring(audioTranscription.indexOf(" "));
+
+                    // Update the CreateView with transcribed data
                     view.getTypeArea().setText(type);
                     view.getIngredientList().setText(IngredientList);
 
@@ -132,7 +150,7 @@ class AudioRecorder extends VBox {
         });
     }
 
-
+    // Method to start recording audio
     private void startRecording() {
         Thread t = new Thread(
                 new Runnable() {
@@ -167,36 +185,22 @@ class AudioRecorder extends VBox {
         t.start();
     }
 
+    // Method to get file data from an audio file
     // return String to be written to output stream on server
     private byte[] GetFileData(File file) throws IOException {
-        // String fileData = "";
-        // try {
-        // fileData += ("Content-Disposition: form-data; name=\"file\"; filename=\"" +
-        //                 file.getName() +
-        //                 "\"\r\n").getBytes();
-        // fileData += ("Content-Type: audio/mpeg\r\n\r\n").getBytes();
 
-        // FileInputStream fileInputStream = new FileInputStream(file);
-        // byte[] buffer = new byte[1024];
-        // int bytesRead;
-        // while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-        //     fileData += bytesRead;
-        // }
-        // fileInputStream.close();
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        //     return null;
-        // }
-        // return fileData;
+         // Create streams for reading the file
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         BufferedInputStream in = new BufferedInputStream(new FileInputStream("Recording.mp3"));
         int read;
+        // Read data into a buffer and write to output stream
         byte[] buff = new byte[1024];
         while ((read = in.read(buff)) > 0)
         {
             out.write(buff, 0, read);
         }
         out.flush();
+        // Convert the output stream to a byte array
         byte[] audioBytes = out.toByteArray();
         //String audioString = new String(audioBytes, java.nio.charset.StandardCharsets.ISO_8859_1);     
         return audioBytes;
@@ -207,3 +211,30 @@ class AudioRecorder extends VBox {
         targetDataLine.close();
     }
 }
+
+
+
+
+
+//---------------- alternative approaches ----------------- //
+// ---------------- saved here in case we need it -----------------//
+
+// String fileData = "";
+// try {
+// fileData += ("Content-Disposition: form-data; name=\"file\"; filename=\"" +
+//                 file.getName() +
+//                 "\"\r\n").getBytes();
+// fileData += ("Content-Type: audio/mpeg\r\n\r\n").getBytes();
+
+// FileInputStream fileInputStream = new FileInputStream(file);
+// byte[] buffer = new byte[1024];
+// int bytesRead;
+// while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+//     fileData += bytesRead;
+// }
+// fileInputStream.close();
+// } catch (Exception e) {
+//     e.printStackTrace();
+//     return null;
+// }
+// return fileData;
